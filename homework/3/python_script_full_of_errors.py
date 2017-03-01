3b6defbe1f0a9dca5ec406c5931e9424a5a4fddf
Kreshel Nguyen <kreshel@utexas.edu>
2017-03-01 08:24:00 -0600

    homework 3

---
+++ homework/3/python_script_full_of_errors.py
@@ -0,0 +1,45 @@
+# syntax error: was using %% instead of %d
+_life_expectancy = 120; print('\n' + "The life expectancy for the millennials is projected to be %d years! (But don't believe it...)" % (_life_expectancy) + '\n');
+
+print('''
+    A recent study published in the journal of Nature, discovered that over the past century,
+    although the life expectancy has significantly increased due to technological advances,
+    the maximum life span of the oldest people in the world has not changed much.
+''')
+
+import cmath # cmath function always return complex numbers
+import math # math function always work with and return real numbers
+print ("""
+Cardano was the first to introduce complex numbers of the form a + sqrt(-b) into algebra, but he had misgivings about it. 
+In his solution to an algebra equation he encountered the solution 5 + sqrt(-15) for the unknown, which is now mathematically represented by \n \n \t
+      {first:} \n\nin Python, which can also be obtained as an addition of real and imaginary numbers in Python like this \n\n\n
+      
+      5 + sqrt(-15) = {second:}, \n\n
+      which can also be manually stated as 
+      
+      
+      {third:} \n
+
+\n
+""".format(second=complex(5,math.sqrt(15)) , first=5+cmath.sqrt(-15) , third=5+3.872983346207417j))
+# semantic error: second was outputting wrong info even if correct syntax
+print('''
+
+One final note: \n
+\tIn python the sqrt function from math and cmath modules are different.
+\tThe sqrt function that returns "float" results is sqrt from math module.
+\tTherefore, if using math module, then,
+
+\t\tsqrt(25) = {:.4f}, 
+
+\twhich is obviously a float (real number).
+
+''' .format(math.sqrt(25)))
+
+print('''
+
+Also note that by convention, 0**0 = {first:d} in Python.
+And division by 0, will give you a runtime exception: 1/0 = {second:}
+
+''' .format(first=0**0,second='NaN'))
+# runtime error: divide by zero
---
+++ homework/3/script.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+print("""This is Python version 3.5.2
+
+Python is the best language for String manipulation!
+
+!noitalupinam gnirtS rof egaugnal tseb eht si nohtyP
+
+!otlpnmgnit o gunlte h inhy
+
+pYTHON IS THE BEST LANGUAGE FOR sTRING MANIPULATION!
+
+
+The sentence 'Python is the best language for String manipulation!' contains
+4 'a' letters, and
+0 'A' letters!
+
+Python\nis\nthe\nbest\nlanguage\nfor\nString\nmanipulation!
+
+PYTHON\nIS\nTHE\nBEST\nLANGUAGE\nFOR\nSTRING\nMANIPULATION!
+""")
